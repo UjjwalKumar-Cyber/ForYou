0aebdb9dc2480cd832ee48bb4fc54b0114b46976
Ujjwal <siryadavujjwal0109@gmail.com>
2026-05-16 14:17:31 +0530

    Fix stale PWA cache for admin page loading

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = "foryou-static-v12";
+const CACHE_NAME = "foryou-static-v13";
 const STATIC_ASSETS = [
-  "/css/styles.css?v=20260516-popups3",
-  "/js/admin.js?v=20260516-popups3",
-  "/js/message.js?v=20260516-popups3",
-  "/js/profile.js?v=20260516-popups3",
-  "/js/secret-login.js?v=20260516-popups3",
+  "/css/styles.css?v=20260516-privacy4",
+  "/js/admin.js?v=20260516-privacy4",
+  "/js/message.js?v=20260516-privacy4",
+  "/js/profile.js?v=20260516-privacy4",
+  "/js/secret-login.js?v=20260516-privacy4",
   "/assets/seal.svg",
   "/manifest.webmanifest"
 ];
